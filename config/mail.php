6cab742e146160580051886ac6efad61830a1b41
linhnd8x <duylinh89@gmail.com>
2016-07-14 10:51:15 +0700

    đ

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => 'nguyenduylinh.ta@gmail.com', 'name' => 'Linh'],
+    'from' => ['address' => 'abc@gmail.com', 'name' => 'Linh'],
 
     /*
     |--------------------------------------------------------------------------
@@ -81,7 +81,7 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME','nguyenduylinh.ta@gmail.com'),
+    'username' => env('MAIL_USERNAME','abc.ta@gmail.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -94,7 +94,7 @@ return [
     |
     */
 
-    'password' => env('MAIL_PASSWORD', 'colenlinh123'),
+    'password' => env('MAIL_PASSWORD', 'password'),
 
     /*
     |--------------------------------------------------------------------------
